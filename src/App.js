6af70ddbeeadcb0e7496409ab21f1b82ab6c867f
Kristin Walker <kristin.walker314@gmail.com>
2022-02-28 00:07:32 -0600

    list filtering works!

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
-import React, { useState } from 'react';
-import ExpenseItemSet from './components/Expenses/ExpenseItemSet';
-import NewExpense from './components/Expenses/NewExpense/NewExpense';
+import React, { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/Expenses/NewExpense/NewExpense";
+
+const INITIAL_EXPENSES = [
+	{
+		id: "e0",
+		title: "Toilet Paper (pre-Covid)",
+		amount: 994.12,
+		date: new Date(2019, 9, 29),
+	},
+	{
+		id: "e1",
+		title: "Toilet Paper (Covid!!!)",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{
+		id: "e2",
+		title: "New TV",
+		amount: 799.49,
+		date: new Date(2021, 2, 12),
+	},
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 2, 27),
+	},
+	{
+		id: "e4",
+		title: "New Desk (Wooden)",
+		amount: 450,
+		date: new Date(2021, 5, 12),
+	},
+	{
+		id: "e5",
+		title: "New Desk (Steel)",
+		amount: 460,
+		date: new Date(2022, 3, 31),
+	},
+	{
+		id: "e6",
+		title: "New Desk (Gold)",
+		amount: 470,
+		date: new Date(2022, 5, 21),
+	},
+];
 
 function App() {
-  const [expenses, setExpenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ]);
+	const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
-  const newExpenseHandler = newExpenseData => {
-    console.log(newExpenseData);
-    setExpenses([...expenses, newExpenseData]);
-  };
+	const addExpenseHandler = (newExpenseData) => {
+		setExpenses((prevExpenses) => {
+			return [newExpenseData, ...prevExpenses];
+		});
+	};
 
-  return (
-    <div id="inner">
-      <h1>Let's get started!</h1>
-      <NewExpense onSubmitNewExpense={newExpenseHandler} />
-      <ExpenseItemSet set={expenses}></ExpenseItemSet>
-    </div>
-  );
+	return (
+		<div id="inner">
+			<h1>Let's get started!</h1>
+			<NewExpense onSubmitNewExpense={addExpenseHandler} />
+			<Expenses items={expenses}></Expenses>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  let date = props.date;
-  let amount = props.amount;
-
-  const clickHandler = () => {
-    setTitle('updated!');
-  };
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={date}></ExpenseDate>
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
-  );
+	return (
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date}></ExpenseDate>
+				<div className="expense-item__description">
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">
+						{"$" + new Intl.NumberFormat("en-US").format(props.amount)}
+					</div>
+				</div>
+			</Card>
+		</li>
+	);
 }
 
 export default ExpenseItem;
